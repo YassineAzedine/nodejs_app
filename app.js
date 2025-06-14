91017a317d31dba95689c9637982c98452ab436a
yassine azedine <yassine.azedine0711@gmail.com>
2025-06-14 15:53:37 +0100

    deployer to aws-ec2

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware pour parser JSON
+app.use(express.json());
+
+// Connexion à MongoDB (change l'URL par la tienne)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connecté à MongoDB'))
+.catch(err => console.error('Erreur MongoDB:', err));
+
+// Définir un schéma et modèle Mongoose
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Route pour ajouter un utilisateur
+app.post('/users', async (req, res) => {
+  try {
+    const user = new User({ name: 'Yassine', email: 'yassine@example.com' });
+
+    await user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// Route pour récupérer tous les utilisateurs
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Serveur lancé sur http://localhost:${port}`);
+});
